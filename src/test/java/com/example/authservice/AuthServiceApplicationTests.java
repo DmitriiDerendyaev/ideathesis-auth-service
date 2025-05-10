c3eaa858fa6db0a249b0d941938791985bb4ad0a
Dmitrii Derendyaev <dds_stalker@mail.ru>
2025-05-10 18:22:21 +0400

    [IDEATHESIS-46] feature: add deploy config to GitHub actions

--- src/test/java/com/example/authservice/AuthServiceApplicationTests.java
+++ src/test/java/com/example/authservice/AuthServiceApplicationTests.java
@@ -10,33 +10,4 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 class AuthServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
-	@Test
-	public void testPasswordMatch() {
-		String rawPassword = "testpass";
-		String encodedPassword = "$2a$10$sjfG2lpmM6/0d78Ot3x9s.BQztadXkz21EolVqtKpdBfZczImXldi";
-
-		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
-		boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
-
-		assertTrue(matches); // Должно быть true
-	}
-
-	@Test
-	public void testPasswordHashing() {
-		String rawPassword = "testpass";
-		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
-
-		// Создаем новый хеш
-		String newEncodedPassword = passwordEncoder.encode(rawPassword);
-		System.out.println("New encoded password: " + newEncodedPassword);
-
-		// Проверяем, совпадает ли новый хеш с текущим
-		boolean matches = passwordEncoder.matches(rawPassword, "$2a$10$sjfG2lpmM6/0d78Ot3x9s.BQztadXkz21EolVqtKpdBfZczImXldi");
-		assertTrue(matches);
-	}
-
 }
